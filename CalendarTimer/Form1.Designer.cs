18a0c22989fab19fb5ea6e2ebe233a39a686f553
Leo Lee <basil1452@protonmail.com>
2023-10-13 21:16:23 +0900

    Timer with Stopwatch

--- CalendarTimer/Form1.Designer.cs
+++ CalendarTimer/Form1.Designer.cs
@@ -28,19 +28,138 @@
         /// </summary>
         private void InitializeComponent()
         {
+            components = new System.ComponentModel.Container();
+            timer1 = new System.Windows.Forms.Timer(components);
+            label1 = new Label();
+            label2 = new Label();
+            tabControl1 = new TabControl();
+            tabPage1 = new TabPage();
+            tabPage2 = new TabPage();
+            tabPage3 = new TabPage();
+            button1 = new Button();
+            button2 = new Button();
+            tabControl1.SuspendLayout();
+            tabPage1.SuspendLayout();
             SuspendLayout();
+            // 
+            // timer1
+            // 
+            timer1.Tick += timer1_Tick;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Arial", 25.8000011F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.Location = new Point(846, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(139, 50);
+            label1.TabIndex = 0;
+            label1.Text = "label1";
+            label1.Click += label1_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.BackColor = Color.White;
+            label2.Font = new Font("맑은 고딕 Semilight", 28.2F, FontStyle.Italic, GraphicsUnit.Point);
+            label2.ForeColor = Color.Black;
+            label2.Location = new Point(253, 128);
+            label2.Name = "label2";
+            label2.Size = new Size(260, 62);
+            label2.TabIndex = 3;
+            label2.Text = "00:00:00:00";
+            // 
+            // tabControl1
+            // 
+            tabControl1.Controls.Add(tabPage1);
+            tabControl1.Controls.Add(tabPage2);
+            tabControl1.Controls.Add(tabPage3);
+            tabControl1.Location = new Point(2, 0);
+            tabControl1.Name = "tabControl1";
+            tabControl1.SelectedIndex = 0;
+            tabControl1.Size = new Size(842, 414);
+            tabControl1.TabIndex = 4;
+            // 
+            // tabPage1
+            // 
+            tabPage1.Controls.Add(button2);
+            tabPage1.Controls.Add(button1);
+            tabPage1.Controls.Add(label2);
+            tabPage1.Location = new Point(4, 29);
+            tabPage1.Name = "tabPage1";
+            tabPage1.Padding = new Padding(3);
+            tabPage1.Size = new Size(834, 381);
+            tabPage1.TabIndex = 0;
+            tabPage1.Text = "StopWatch";
+            tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            tabPage2.Location = new Point(4, 29);
+            tabPage2.Name = "tabPage2";
+            tabPage2.Padding = new Padding(3);
+            tabPage2.Size = new Size(834, 381);
+            tabPage2.TabIndex = 1;
+            tabPage2.Text = "World Time";
+            tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // tabPage3
+            // 
+            tabPage3.Location = new Point(4, 29);
+            tabPage3.Name = "tabPage3";
+            tabPage3.Padding = new Padding(3);
+            tabPage3.Size = new Size(834, 381);
+            tabPage3.TabIndex = 2;
+            tabPage3.Text = "tabPage3";
+            tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(155, 263);
+            button1.Name = "button1";
+            button1.Size = new Size(149, 89);
+            button1.TabIndex = 4;
+            button1.Text = "Start";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(509, 275);
+            button2.Name = "button2";
+            button2.Size = new Size(161, 77);
+            button2.TabIndex = 5;
+            button2.Text = "Stop";
+            button2.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(9F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1043, 521);
+            ClientSize = new Size(1341, 695);
+            Controls.Add(tabControl1);
+            Controls.Add(label1);
+            Margin = new Padding(4);
             Name = "Form1";
             Text = "Form1";
             Load += Form1_Load;
+            tabControl1.ResumeLayout(false);
+            tabPage1.ResumeLayout(false);
+            tabPage1.PerformLayout();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private Label label1;
+        private Label label2;
+        private TabControl tabControl1;
+        private TabPage tabPage1;
+        private TabPage tabPage2;
+        private TabPage tabPage3;
+        private Button button1;
+        private Button button2;
     }
 }
--- CalendarTimer/Form1.cs
+++ CalendarTimer/Form1.cs
@@ -1,13 +1,33 @@
+using System.Diagnostics;
+
 namespace CalendarTimer
 {
     public partial class Form1 : Form
     {
+        private Stopwatch stopwatch = new Stopwatch();
         public Form1()
         {
             InitializeComponent();
         }
 
         private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            //Set Time as Current Time
+            //Timer is necessary to display digital clock
+            label1.Text = DateTime.Now.ToString("HH:mm:ss \n yyyy-MM-dd dddd");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
         {
 
         }
