1dd878a44f9d13b315e18799d83c9f9086a6f0e4
Leo Lee <bonaparte_19th@hotmail.com>
2023-10-14 22:39:03 +0900

    Writing StopWatch code : 100%(Completed)

--- CalendarTimer/Form1.Designer.cs
+++ CalendarTimer/Form1.Designer.cs
@@ -30,44 +30,51 @@
         {
             components = new System.ComponentModel.Container();
             timer1 = new System.Windows.Forms.Timer(components);
-            label1 = new Label();
-            label2 = new Label();
+            CurTime = new Label();
+            StopWatchLabel = new Label();
             tabControl1 = new TabControl();
             tabPage1 = new TabPage();
+            ResetBtn = new Button();
+            StopBtn = new Button();
+            StartBtn = new Button();
             tabPage2 = new TabPage();
             tabPage3 = new TabPage();
-            button1 = new Button();
-            button2 = new Button();
             tabControl1.SuspendLayout();
             tabPage1.SuspendLayout();
             SuspendLayout();
             // 
             // timer1
             // 
-            timer1.Tick += timer1_Tick;
+            timer1.Tick += Timer1_tick;
             // 
-            // label1
+            // CurTime
             // 
-            label1.AutoSize = true;
-            label1.Font = new Font("Arial", 25.8000011F, FontStyle.Regular, GraphicsUnit.Point);
-            label1.Location = new Point(846, 9);
-            label1.Name = "label1";
-            label1.Size = new Size(139, 50);
-            label1.TabIndex = 0;
-            label1.Text = "label1";
-            label1.Click += label1_Click;
+            CurTime.AutoSize = true;
+            CurTime.BackColor = Color.Wheat;
+            CurTime.BorderStyle = BorderStyle.FixedSingle;
+            CurTime.Font = new Font("Arial", 25.8000011F, FontStyle.Regular, GraphicsUnit.Point);
+            CurTime.Location = new Point(897, 24);
+            CurTime.Margin = new Padding(2, 0, 2, 0);
+            CurTime.Name = "CurTime";
+            CurTime.Size = new Size(107, 42);
+            CurTime.TabIndex = 0;
+            CurTime.Text = "label1";
+            CurTime.TextAlign = ContentAlignment.MiddleCenter;
+            CurTime.Click += label1_Click;
             // 
-            // label2
+            // StopWatchLabel
             // 
-            label2.AutoSize = true;
-            label2.BackColor = Color.White;
-            label2.Font = new Font("맑은 고딕 Semilight", 28.2F, FontStyle.Italic, GraphicsUnit.Point);
-            label2.ForeColor = Color.Black;
-            label2.Location = new Point(253, 128);
-            label2.Name = "label2";
-            label2.Size = new Size(260, 62);
-            label2.TabIndex = 3;
-            label2.Text = "00:00:00:00";
+            StopWatchLabel.AutoSize = true;
+            StopWatchLabel.BackColor = Color.White;
+            StopWatchLabel.Font = new Font("맑은 고딕 Semilight", 28.2F, FontStyle.Italic, GraphicsUnit.Point);
+            StopWatchLabel.ForeColor = Color.Black;
+            StopWatchLabel.Location = new Point(246, 101);
+            StopWatchLabel.Margin = new Padding(2, 0, 2, 0);
+            StopWatchLabel.Name = "StopWatchLabel";
+            StopWatchLabel.Size = new Size(160, 51);
+            StopWatchLabel.TabIndex = 3;
+            StopWatchLabel.Text = "00:00:00";
+            StopWatchLabel.Click += StopWatchLabel_Click;
             // 
             // tabControl1
             // 
@@ -75,72 +82,95 @@
             tabControl1.Controls.Add(tabPage2);
             tabControl1.Controls.Add(tabPage3);
             tabControl1.Location = new Point(2, 0);
+            tabControl1.Margin = new Padding(2);
             tabControl1.Name = "tabControl1";
             tabControl1.SelectedIndex = 0;
-            tabControl1.Size = new Size(842, 414);
+            tabControl1.Size = new Size(875, 492);
             tabControl1.TabIndex = 4;
             // 
             // tabPage1
             // 
-            tabPage1.Controls.Add(button2);
-            tabPage1.Controls.Add(button1);
-            tabPage1.Controls.Add(label2);
-            tabPage1.Location = new Point(4, 29);
-            tabPage1.Name = "tabPage1";
-            tabPage1.Padding = new Padding(3);
-            tabPage1.Size = new Size(834, 381);
+            tabPage1.Controls.Add(ResetBtn);
+            tabPage1.Controls.Add(StopBtn);
+            tabPage1.Controls.Add(StartBtn);
+            tabPage1.Controls.Add(StopWatchLabel);
+            tabPage1.Location = new Point(4, 24);
+            tabPage1.Margin = new Padding(2);
+            tabPage1.Name = "StopWatch";
+            tabPage1.Padding = new Padding(2);
+            tabPage1.Size = new Size(700, 409);
             tabPage1.TabIndex = 0;
             tabPage1.Text = "StopWatch";
             tabPage1.UseVisualStyleBackColor = true;
             // 
+            // ResetBtn
+            // 
+            ResetBtn.BackColor = Color.AliceBlue;
+            ResetBtn.Location = new Point(455, 228);
+            ResetBtn.Name = "ResetBtn";
+            ResetBtn.Size = new Size(116, 67);
+            ResetBtn.TabIndex = 6;
+            ResetBtn.Text = "Reset";
+            ResetBtn.UseVisualStyleBackColor = false;
+            ResetBtn.Click += ResetBtn_Click;
+            // 
+            // StopBtn
+            // 
+            StopBtn.BackColor = Color.PeachPuff;
+            StopBtn.Location = new Point(246, 228);
+            StopBtn.Margin = new Padding(2);
+            StopBtn.Name = "StopBtn";
+            StopBtn.Size = new Size(116, 67);
+            StopBtn.TabIndex = 5;
+            StopBtn.Text = "Stop";
+            StopBtn.UseVisualStyleBackColor = false;
+            StopBtn.Click += StopBtn_Click;
+            // 
+            // StartBtn
+            // 
+            StartBtn.BackColor = Color.LightCoral;
+            StartBtn.Location = new Point(70, 228);
+            StartBtn.Margin = new Padding(2);
+            StartBtn.Name = "StartBtn";
+            StartBtn.Size = new Size(116, 67);
+            StartBtn.TabIndex = 4;
+            StartBtn.Text = "Start";
+            StartBtn.UseVisualStyleBackColor = false;
+            StartBtn.Click += StartBtn_Click;
+            // 
             // tabPage2
             // 
-            tabPage2.Location = new Point(4, 29);
-            tabPage2.Name = "tabPage2";
-            tabPage2.Padding = new Padding(3);
-            tabPage2.Size = new Size(834, 381);
+            tabPage2.Location = new Point(4, 24);
+            tabPage2.Margin = new Padding(2);
+            tabPage2.Name = "WorldTime";
+            tabPage2.Padding = new Padding(2);
+            tabPage2.Size = new Size(867, 464);
             tabPage2.TabIndex = 1;
             tabPage2.Text = "World Time";
             tabPage2.UseVisualStyleBackColor = true;
             // 
             // tabPage3
             // 
-            tabPage3.Location = new Point(4, 29);
-            tabPage3.Name = "tabPage3";
-            tabPage3.Padding = new Padding(3);
-            tabPage3.Size = new Size(834, 381);
+            tabPage3.Location = new Point(4, 24);
+            tabPage3.Margin = new Padding(2);
+            tabPage3.Name = "Alarm";
+            tabPage3.Padding = new Padding(2);
+            tabPage3.Size = new Size(700, 409);
             tabPage3.TabIndex = 2;
-            tabPage3.Text = "tabPage3";
+            tabPage3.Text = "Alarm";
             tabPage3.UseVisualStyleBackColor = true;
-            // 
-            // button1
-            // 
-            button1.Location = new Point(155, 263);
-            button1.Name = "button1";
-            button1.Size = new Size(149, 89);
-            button1.TabIndex = 4;
-            button1.Text = "Start";
-            button1.UseVisualStyleBackColor = true;
-            button1.Click += button1_Click;
-            // 
-            // button2
-            // 
-            button2.Location = new Point(509, 275);
-            button2.Name = "button2";
-            button2.Size = new Size(161, 77);
-            button2.TabIndex = 5;
-            button2.Text = "Stop";
-            button2.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(9F, 20F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1341, 695);
+            AutoSize = true;
+            ClientSize = new Size(1043, 521);
             Controls.Add(tabControl1);
-            Controls.Add(label1);
-            Margin = new Padding(4);
+            Controls.Add(CurTime);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
             Name = "Form1";
+            RightToLeftLayout = true;
             Text = "Form1";
             Load += Form1_Load;
             tabControl1.ResumeLayout(false);
@@ -153,13 +183,14 @@
         #endregion
 
         private System.Windows.Forms.Timer timer1;
-        private Label label1;
-        private Label label2;
+        private Label CurTime;
+        private Label StopWatchLabel;
         private TabControl tabControl1;
         private TabPage tabPage1;
         private TabPage tabPage2;
         private TabPage tabPage3;
-        private Button button1;
-        private Button button2;
+        private Button StartBtn;
+        private Button StopBtn;
+        private Button ResetBtn;
     }
 }
--- CalendarTimer/Form1.cs
+++ CalendarTimer/Form1.cs
@@ -1,10 +1,18 @@
 using System.Diagnostics;
+using System.Timers;
+using static System.Timers.Timer;
 
 namespace CalendarTimer
 {
     public partial class Form1 : Form
     {
-        private Stopwatch stopwatch = new Stopwatch();
+        //Stopwatch Class is used to measure performance time, not pure stopwatch
+        //PC Win Form only needs Hour, minute, and Second
+        private int hour;
+        private int minute;
+        private int second;
+        private System.Timers.Timer swTimer;
+
         public Form1()
         {
             InitializeComponent();
@@ -12,6 +20,13 @@ namespace CalendarTimer
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            swTimer = new System.Timers.Timer();
+            //Interval : If you want to make stopwatch for people like runner
+            //swTimer.Interval = 1 : Set Stopwatch Interval as 1 miliseconds
+            //swTimer.Interval = 1000 : Set Stopwatch Interval as 1 seconds
+            swTimer.Interval = 1000;
+            swTimer.Enabled = true;
+            swTimer.Elapsed += SwEvent;
             timer1.Start();
         }
 
@@ -20,14 +35,63 @@ namespace CalendarTimer
 
         }
 
-        private void timer1_Tick(object sender, EventArgs e)
+        private void Timer1_tick(object sender, EventArgs e)
         {
             //Set Time as Current Time
             //Timer is necessary to display digital clock
-            label1.Text = DateTime.Now.ToString("HH:mm:ss \n yyyy-MM-dd dddd");
+            CurTime.Text = DateTime.Now.ToString("HH:mm:ss \n yyyy-MM-dd dddd");
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void StartBtn_Click(object sender, EventArgs e)
+        {
+            swTimer.Start();
+        }
+
+        //Subscribe the Event(Stopwatch Timer Elapse)
+        private void SwEvent(object sender, ElapsedEventArgs e)
+        {
+            //Invoke : Apporaching method from Other Threads
+            Invoke(new Action(() =>
+            {
+                second++;
+
+                if (second == 60)
+                {
+                    second = 0;
+                    minute++;
+                }
+                if (minute == 60)
+                {
+                    minute = 0;
+                    hour++;
+                }
+
+                //Show the Elapsed Time
+                ShowSwTime(hour, minute, second);
+            }));
+        }
+
+
+        private void StopBtn_Click(object sender, EventArgs e)
+        {
+            swTimer.Stop();
+        }
+
+        private void ResetBtn_Click(object sender, EventArgs e)
+        {
+            swTimer.Stop();
+            ShowSwTime(hour = 0, minute = 0, second = 0);
+        }
+
+        private void ShowSwTime(int hour, int minute, int second)
+        {
+            StopWatchLabel.Text = String.Format("{0}:{1}:{2}",
+                    hour.ToString().PadLeft(2, '0'),
+                    minute.ToString().PadLeft(2, '0'),
+                    second.ToString().PadLeft(2, '0'));
+        }
+
+        private void StopWatchLabel_Click(object sender, EventArgs e)
         {
 
         }
